d025216ffa09c27e1879b299e94f528bc9226c63
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-01 18:06:55 +0900

    move sphere by keyboard input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+}
