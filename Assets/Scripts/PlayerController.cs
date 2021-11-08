219e8b6479d5b197e873c938b58d03b915eaeeb8
smptmhr <tomohiro.shimpo@gmail.com>
2021-11-08 20:14:05 +0900

    変数の一部をローカル変数に変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,8 +13,6 @@ public class PlayerController : MonoBehaviour {
     public float time;
     private int count;
     private bool isClear;
-    public float moveHorizontal;
-    public float moveVertical;
 
     // Start is called before the first frame update
     void Start () {
@@ -29,8 +27,8 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        moveHorizontal = Input.GetAxis ("Horizontal");
-        moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
         rb.AddForce (movement * speed);
